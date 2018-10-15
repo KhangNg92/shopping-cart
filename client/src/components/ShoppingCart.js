a5d75798c5b5e386510e068eafbe74ee60a754e9
Khang Nguyen <mkavo92@gmail.com>
2018-10-15 12:31:17 -0500

    progress

---
+++ client/src/components/ShoppingCart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Header from './Header';
+import Account from './Account';
+import Cart from './Cart';
+import Homepage from './Homepage';
+import ProductDetails from './ProductDetails';
+import Footer from './Footer';
+import NotFound from './NotFound';
+import '../styles/ShoppingCart.css';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
+
+const ShoppingCart = () => (
+  <BrowserRouter>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <div className="container">
+        <Route component={Header} />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/account" component={Account} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/product/:id" component={ProductDetails} />
+          <Route component={NotFound} />
+        </Switch>
+        <Route component={Footer} />
+      </div>
+    </MuiThemeProvider>
+  </BrowserRouter>
+);
+
+export default ShoppingCart;
